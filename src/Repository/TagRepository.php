2be583e23a91dd692d771afdd4873b83d9bff7cf
1474249 <alexis.vuk@gmail.com>
2025-07-26 00:06:59 +0200

    add cache to tag repo

--- src/Repository/TagRepository.php
+++ src/Repository/TagRepository.php
@@ -5,19 +5,45 @@ namespace App\Repository;
 use App\Entity\Tag;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 /**
  * @extends ServiceEntityRepository<Tag>
  */
 class TagRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    private $cache;
+    private $logger;
+
+    public function __construct(ManagerRegistry $registry, CacheInterface $cache, LoggerInterface $logger)
     {
         parent::__construct($registry, Tag::class);
+        $this->cache = $cache;
+        $this->logger = $logger;
     }
 
     public function findAllOrderedByName(): array
     {
-        return $this->findBy([], ['name' => 'ASC']);
+        $cacheKey = 'all_objects_ordered_by_name';
+
+        // Récupérer les données du cache
+        $data = $this->cache->get($cacheKey, function(ItemInterface $item) {
+            $cacheKey = 'all_objects_ordered_by_name';
+
+            $item->expiresAfter(3600);
+            $result = $this->findBy([], ['name' => 'ASC']);
+
+            // Logger le contenu qui vient d'être mis en cache
+            $this->logger->info('Cache mis à jour pour la clé: ' . $cacheKey, ['data' => $result]);
+
+            return $result;
+        });
+
+        // Logger les données récupérées du cache
+        $this->logger->info('Données récupérées du cache pour la clé: ' . $cacheKey, ['data' => $data]);
+
+        return $data;
     }
 }
